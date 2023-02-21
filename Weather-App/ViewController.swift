2f12a8cbb539f6f149f15e0e849adc5e08ec2c1e
sejal <sejal4789@gmail.com>
2023-02-22 00:45:49 +0530

    Displayed Static Weather Info.

--- Weather-App/ViewController.swift
+++ Weather-App/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var rainLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var imageWatherView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageWatherView.image = UIImage(named: "Morning")
     }
 
-
+    @IBAction func morningTap(_ sender: Any) {
+        tempLabel.text = "Temperature is 15"
+        rainLabel.text = "Not Raining"
+        timeLabel.text = "7a.m."
+        imageWatherView.image = UIImage(named: "Morning")
+    }
+    
+    @IBAction func dayTap(_ sender: Any) {
+        tempLabel.text = "Temperature is 30"
+        rainLabel.text = "Not Raining"
+        timeLabel.text = "1pm"
+        imageWatherView.image = UIImage(named: "afternoon")
+    }
+    
+    @IBAction func AfternonTap(_ sender: Any) {
+        tempLabel.text = "Temperature is 18"
+        rainLabel.text = "Raining"
+        timeLabel.text = "8p.m."
+        imageWatherView.image = UIImage(named: "night")
+    }
 }
 
